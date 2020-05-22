325cb3623ba78ff01ac8c2b6a3bb6c87bea7e868
Skalour <marceau.hollertt@hotmail.fr>
2020-05-22 13:30:13 +0200

    On GitHub

---
+++ Bluetooth/MacPeripheral.swift
@@ -0,0 +1,15 @@
+//
+//  MacPeripheral.swift
+//  Bluetooth
+//
+//  Created by Marceau Hollertt on 22/05/2020.
+//  Copyright © 2020 Marceau Hollertt. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class MacPeripheral: NSObject{
+    public static let macPeripheralUUID = CBUUID.init(string: "9B225DF4-3B9A-2332-4BB9-1D8CD469B686")
+
+}
--- Bluetooth/ViewController.swift
+++ Bluetooth/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBPeripheralDelegate, CBCentralManagerDelegate {
+   private var centralManager: CBCentralManager!
+   private var peripheral: CBPeripheral!
+    private var int = 0
+   
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("Central state update")
+                  if central.state != .poweredOn {
+                      print("Central is not powered on")
+                  } else {
+                      print("Central scanning for devices");
+                    centralManager.scanForPeripherals(withServices: nil,
+                                                        options: [CBCentralManagerScanOptionAllowDuplicatesKey : false])
+                  }
+    }
+    
+
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if(int < 20){
+            //print(peripheral)
+            if let power = advertisementData[CBAdvertisementDataTxPowerLevelKey] as? Double{
+                       print("RSSI : ", RSSI)
+                       print("Distance to peripheral ",peripheral.name, " is ", pow(10, ((power - Double(truncating: RSSI))/20)))
+                   }
+            int = int + 1
+        }
+        else{
+            self.centralManager.stopScan()
+
+        }
+
+        self.peripheral = peripheral
+        self.peripheral.delegate = self
+
+        self.centralManager.connect(self.peripheral, options: nil)
+
+    }
+    
+    // The handler if we do connect succesfully
+        func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+            if peripheral == self.peripheral {
+                print(peripheral.name)
+                print("Connected !")
+
+            }
+        }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.global())
+        func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        }
     }
 
 
